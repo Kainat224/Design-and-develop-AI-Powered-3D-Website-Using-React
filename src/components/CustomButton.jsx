f335c232eeda521dc9cc501ba70d0a1d8ad74808
Kainat <anmolkainat244@gmail.com>
2023-04-10 12:11:20 -0700

    New Changes

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomButton = () => {
+  return <button>CustomBtn</button>;
+};
+
+export default CustomButton;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { useSnapshot } from "valtio";
 import state from "../store";
+import { CustomButton } from "../components";
 
 import {
   headContainerAnimation,
@@ -22,6 +23,28 @@ const Home = () => {
               className="w-8 h-8 object-contain"
             />
           </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <h1 className="head-text">
+              Let's <br className="xl:block hidden" />
+              do it!
+            </h1>
+          </motion.div>
+          <motion.div
+            {...headContainerAnimation}
+            className="flex flex-col gap-5"
+          >
+            <p className="max-w-md font-normal text-gray-600 text-base">
+              Creat your unique and exclusive shirts. With our brand new 3D
+              customization tool.<strong>Unleash your imaginations</strong> and
+              define your own style.
+            </p>
+            <CustomButton
+              type="filled"
+              title="Cutomize It"
+              handleClick={() => (state.intro = false)}
+              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+            />
+          </motion.div>
         </motion.section>
       )}
     </AnimatePresence>
