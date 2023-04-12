ae4b159e19d48fa0cefc2e501d1683ead2b803bd
Kainat <anmolkainat244@gmail.com>
2023-04-12 14:19:45 -0700

    Customize Button

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useSnapshot } from "valtio";
+import state from "../store";
 
-const CustomButton = () => {
-  return <button>CustomBtn</button>;
+const CustomButton = ({ type, title, handleClick, customStyles }) => {
+  const snap = useSnapshot(state);
+  const generateStyle = (type) => {
+    if (type === "filled") {
+      return {
+        backgroundColor: "red",
+        color: "#fff",
+      };
+    }
+  };
+
+  return (
+    <button
+      className={`px-2 py-1.5 flex-1 round-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
+  );
 };
 
 export default CustomButton;
