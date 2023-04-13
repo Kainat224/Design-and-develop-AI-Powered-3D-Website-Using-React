ff09bf86805b15aeeb9f592b04d74ea9853f59a5
Kainat <anmolkainat244@gmail.com>
2023-04-13 05:22:51 -0700

    New Changes to customizer

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import { AnimatePresence } from "framer-motion";
-import { useSnapshot } from "valtio";
-import { config } from "../config/config";
 import state from "../store";
+import { useSnapshot } from "valtio";
+
 import { download } from "../assets";
 import { downloadCanvasToImage, reader } from "../config/helpers";
 import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
@@ -15,8 +16,31 @@ import {
   Tab,
 } from "../components";
 
-const customizer = () => {
-  return <div>customizer</div>;
+const Customizer = () => {
+  const snap = useSnapshot(state);
+  return (
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key="custom"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation("left")}
+          >
+            <div className="flex items-center min-h-screen">
+              <div className="editortabs-container tabs">
+                {EditorTabs.map((tab) => (
+                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+
+          <motion.div className="absolute z-10 top-5 right-5"></motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  );
 };
 
-export default customizer;
+export default Customizer;
